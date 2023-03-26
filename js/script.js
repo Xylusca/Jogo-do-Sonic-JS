2100a1c6a40a800ab7b28988235ec195a2cdf891
Xylusca <lukas_sud@live.com>
2023-03-26 01:39:32 -0300

    utilizando tanto o espaço quanto o click para pulo

--- js/script.js
+++ js/script.js
@@ -23,105 +23,114 @@ const jump = () => {
    }, 500);
 }
 
+// Função para lidar com o pulo do Sonic
+const handleJump = (event) => {
+   // Verificando se o evento foi acionado pela tecla espaço ou pelo clique do botão esquerdo do mouse
+   if (event.code === 'Space' || event.type === 'click') {
+      jump(); // chamando a função jump se a barra de espaço foi pressionada ou se houve um clique
+   }
+};
+
 // Adicionando um event listener para o evento 'keydown' do objeto document
-document.addEventListener('keydown', (event) => {
-   // Verificando se a tecla pressionada foi a barra de espaço
-   if (event.code === 'Space') {
-      jump(); // chamando a função jump se a barra de espaço foi pressionada
-   }
+document.addEventListener('keydown', handleJump);
 
-   // Selecionando todos os elementos HTML com as classes 'nuvens', 'arvores', 'sonic' e 'vilao' e colocando a animação em execução novamente
-   var elements = document.querySelectorAll('.nuvens, .arvores, .sonic, .vilao');
-   for (var i = 0; i < elements.length; i++) {
-      elements[i].style.animationPlayState = 'running';
-   }
-
-   // Definindo uma função anônima para ser executada a cada 10 milissegundos por meio da função setInterval
-   const loop = setInterval(() => {
-      // Obtendo a posição horizontal dos elementos HTML correspondentes em relação à borda esquerda da janela de visualização
-      const vilaoPosition = vilao.offsetLeft;
-      const arvoresPosition = arvores.offsetLeft;
-      const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
-      // Imprimindo a posição do vilão no console para fins de depuração
-      console.log(vilaoPosition)
-      // Verificando se o Sonic colidiu com o vilão
-      if (vilaoPosition < 100 && vilaoPosition > -28 && sonicPosition < 55) {
-         // Selecionando o elemento HTML do modal e o botão "jogar novamente"
-         const modal = document.querySelector('.modal');
-         const restartBtn = document.querySelector('.restart-btn');
-
-         // Função que exibe o modal e pausa o jogo
-         const showGameOver = () => {
-            // Pausando as animações dos elementos HTML
-            vilao.style.animationPlayState = 'paused';
-            arvores.style.animationPlayState = 'paused';
-            sonic.style.animationPlayState = 'paused';
-
-            // Exibindo o modal com a mensagem de "game over" e a pontuação do jogador
-            const steps = +stepsCounter.textContent;
-            const message = modal.querySelector('.message');
-            message.querySelector('.steps').textContent = steps;
-            modal.classList.add('show');
-         }
-
-         // Adicionando um event listener para o botão "jogar novamente"
-         restartBtn.addEventListener('click', () => {
-            // Recarregando a página para reiniciar o jogo
-            location.reload();
-         });
-
-         // Verificando se o Sonic colidiu com o vilão
-         if (vilaoPosition < 100 && vilaoPosition > -28 && sonicPosition < 55) {
-            // ...
-            // Exibindo o modal de "game over" em vez do alert
-            showGameOver();
-         }
+// Adicionando um event listener para o evento 'click' do objeto document
+document.addEventListener('click', handleJump);
 
 
-         // Pausando as animações dos elementos HTML após a colisão
-         vilao.style.animation = 'none';
-         vilao.style.left = vilaoPosition + "px";
+// Selecionando todos os elementos HTML com as classes 'nuvens', 'arvores', 'sonic' e 'vilao' e colocando a animação em execução novamente
+var elements = document.querySelectorAll('.nuvens, .arvores, .sonic, .vilao');
+for (var i = 0; i < elements.length; i++) {
+   elements[i].style.animationPlayState = 'running';
+}
 
-         sonic.style.animation = 'none';
-         sonic.style.bottom = sonicPosition + "px";
+// Definindo uma função anônima para ser executada a cada 10 milissegundos por meio da função setInterval
+const loop = setInterval(() => {
+   // Obtendo a posição horizontal dos elementos HTML correspondentes em relação à borda esquerda da janela de visualização
+   const vilaoPosition = vilao.offsetLeft;
+   const arvoresPosition = arvores.offsetLeft;
+   const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
+   // Imprimindo a posição do vilão no console para fins de depuração
+   // console.log(vilaoPosition)
 
-         arvores.style.animation = 'none';
-         arvores.style.left = arvoresPosition + "px";
+   // Verificando se o Sonic colidiu com o vilão
+   if (vilaoPosition < 100 && vilaoPosition > -28 && sonicPosition < 55) {
+      // Selecionando o elemento HTML do modal e o botão "jogar novamente"
+      const modal = document.querySelector('.modal');
+      const restartBtn = document.querySelector('.restart-btn');
 
-         // Trocando as imagens dos elementos HTML para imagens estáticas de "parada"
-         vilao.src = 'img/vilao_parado.png';
-         sonic.style.width = '90px';
-         sonic.style.marinleft = '90px';
+      // Função que exibe o modal e pausa o jogo
+      const showGameOver = () => {
+         // Pausando as animações dos elementos HTML
+         vilao.style.animationPlayState = 'paused';
+         arvores.style.animationPlayState = 'paused';
+         sonic.style.animationPlayState = 'paused';
 
-         sonic.src = 'img/sonic_dead.png';
-         sonic.style.width = '90px';
-         sonic.style.marinleft = '90px';
-
-         elements[1].style.animationPlayState = 'paused';
-
-         // Parando a execução da função setInterval
-         clearInterval(loop);
-
-         // Obtendo a pontuação atual do jogador
+         // Exibindo o modal com a mensagem de "game over" e a pontuação do jogador
          const steps = +stepsCounter.textContent;
-
-         // Verificando se a pontuação atual é maior do que a pontuação recorde
-         if (steps > record) {
-            // Armazenando a nova pontuação recorde em localStorage
-            localStorage.setItem('record', steps);
-
-            // Atualizando a variável 'record' e o elemento HTML correspondente
-            record = steps;
-            recordCounter.textContent = record;
-         }
-
+         const message = modal.querySelector('.message');
+         message.querySelector('.steps').textContent = steps;
+         modal.classList.add('show');
       }
 
-      // Incrementando a pontuação do jogador em 1 a cada iteração do loop
-      stepsCounter.textContent = +stepsCounter.textContent + 1;
+      // Adicionando um event listener para o botão "jogar novamente"
+      restartBtn.addEventListener('click', () => {
+         // Recarregando a página para reiniciar o jogo
+         location.reload();
+      });
 
-   }, 100);
-});
+      // Verificando se o Sonic colidiu com o vilão
+      if (vilaoPosition < 100 && vilaoPosition > -28 && sonicPosition < 55) {
+         // ...
+         // Exibindo o modal de "game over" em vez do alert
+         showGameOver();
+      }
+
+
+      // Pausando as animações dos elementos HTML após a colisão
+      vilao.style.animation = 'none';
+      vilao.style.left = vilaoPosition + "px";
+
+      sonic.style.animation = 'none';
+      sonic.style.bottom = sonicPosition + "px";
+
+      arvores.style.animation = 'none';
+      arvores.style.left = arvoresPosition + "px";
+
+      // Trocando as imagens dos elementos HTML para imagens estáticas de "parada"
+      vilao.src = 'img/vilao_parado.png';
+      sonic.style.width = '90px';
+      sonic.style.marinleft = '90px';
+
+      sonic.src = 'img/sonic_dead.png';
+      sonic.style.width = '90px';
+      sonic.style.marinleft = '90px';
+
+      elements[1].style.animationPlayState = 'paused';
+
+      // Parando a execução da função setInterval
+      clearInterval(loop);
+
+      // Obtendo a pontuação atual do jogador
+      const steps = +stepsCounter.textContent;
+
+      // Verificando se a pontuação atual é maior do que a pontuação recorde
+      if (steps > record) {
+         // Armazenando a nova pontuação recorde em localStorage
+         localStorage.setItem('record', steps);
+
+         // Atualizando a variável 'record' e o elemento HTML correspondente
+         record = steps;
+         recordCounter.textContent = record;
+      }
+
+   }
+
+   // Incrementando a pontuação do jogador em 1 a cada iteração do loop
+   stepsCounter.textContent = +stepsCounter.textContent + 1;
+
+}, 100);
+;
 
 
 document.addEventListener('keydown', jump);
